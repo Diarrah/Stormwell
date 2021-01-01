5bee005a840bc0dca3fd3d92d4bbcfaca96cab2f
Diarrah <diarrahcasiano@gmail.com>
2020-12-31 19:57:04 -0800

    -

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+    <div className="footer">
+        <p>Drink Responsibly.</p>
+        <small className="copyright">© 2021. All Rights Reserved.</small>
+    </div>
+)
+
+export default Footer
