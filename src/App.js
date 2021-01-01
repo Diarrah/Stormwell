f491bb796245ebef293c18ad3f0023047596b173
Diarrah <diarrahcasiano@gmail.com>
2020-12-31 19:47:15 -0800

    Context provider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useState, useEffect, useRef } from 'react';
+import { Navbar, Hero, About, Recipes, Contact, Footer } from './components';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const StormwellContext = createContext();
+
+const App = () => {
+    const navbarEl = useRef(),
+          heroEl = useRef(),
+          aboutEl = useRef(),
+          recipeEl = useRef(),
+          contactEl = useRef();
+    const [viewport, setViewport] = useState(window.innerWidth);
+
+        function useWindowSize() {
+            useEffect(() => {
+                function updateSize() {
+                    setViewport(window.innerWidth);
+                }      
+                window.addEventListener("resize", updateSize);
+                updateSize();
+                return () => window.removeEventListener("resize", updateSize);
+            }, []);
+    
+            return viewport;
+        }
+        
+        useWindowSize()
+
+    return (
+        <StormwellContext.Provider value={{navbarEl, heroEl, aboutEl, recipeEl, contactEl, viewport}}>
+            <div className="App">
+                <Navbar />
+                <Hero />
+                <About />
+                <Recipes />
+                <Contact />
+                <Footer />
+            </div>
+        </StormwellContext.Provider>
+    );
 }
 
 export default App;
